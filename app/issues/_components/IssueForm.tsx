d5b14ac0a7d4e836922849e6ae82118c3e015f15
Divine Charles <officialkb17@gmail.com>
2024-03-09 22:02:14 -0800

    Remove duplicate skeletons

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -39,7 +39,7 @@ const IssueForm = ({ issue }: Props) => {
       setisSubmitting(true);
       if (issue) await axios.patch('/api/issues/' + issue.id, data);
       else await axios.post('/api/issues', data);
-      router.push('/issues');
+      router.push('/issues/list');
       router.refresh();
     } catch (error) {
       setisSubmitting(false);
