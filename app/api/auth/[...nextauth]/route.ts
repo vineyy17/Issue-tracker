49a42c762a3fe52418f347e4b3b45e20595091b9
Divine Charles <officialkb17@gmail.com>
2024-03-17 10:40:51 +0100

    Secure the application

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,6 @@
+import authOptions from '@/app/auth/authOptions';
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/prisma/client';
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  theme: {
-    colorScheme: 'light',
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,11 +2,16 @@ import { issueSchema } from '@/app/validationSchemas';
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
 import { error } from 'console';
+import authOptions from '@/app/auth/authOptions';
+import { getServerSession } from 'next-auth';
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } },
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
@@ -35,6 +40,9 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } },
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
